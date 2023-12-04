bb6cd9fe78d2fe420f7061c8487f8609d61b514f
Nino GUEGUEN <ninogueguen@gmail.com>
2023-12-04 12:51:35 +0100

    Réalisation du header du tableau, input de type text et range reliés

--- src/components/GlobalChart.js
+++ src/components/GlobalChart.js
@@ -21,22 +21,38 @@ const GlobalChart = ({ coinsData }) => {
     }
   };
 
+  const excludeCoin = (coin) => {
+    if (
+      coin === "usdt" ||
+      coin === "usdc" ||
+      coin === "busd" ||
+      coin === "dai" ||
+      coin === "ust"
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
   useEffect(() => {
     let chartData = [];
 
     if (coinsData.length > 0) {
-      for (let i = 0; i < 10; i++) {
-        chartData.push({
-          name:
-            coinsData[i].symbol.toUpperCase() +
-            " " +
-            coinsData[i].price_change_percentage_24h_in_currency.toFixed(2) +
-            "%",
-          size: coinsData[i].market_cap,
-          fill: colorPicker(
-            coinsData[i].price_change_percentage_24h_in_currency
-          ),
-        });
+      for (let i = 0; i < 35; i++) {
+        if (excludeCoin(coinsData[i].symbol)) {
+          chartData.push({
+            name:
+              coinsData[i].symbol.toUpperCase() +
+              " | " +
+              coinsData[i].market_cap_change_percentage_24h.toFixed(2) +
+              "%",
+            size: coinsData[i].market_cap,
+            fill: colorPicker(
+              coinsData[i].price_change_percentage_24h_in_currency
+            ),
+          });
+        }
       }
     }
     setDataArray(chartData);
