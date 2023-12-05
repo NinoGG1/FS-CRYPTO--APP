8f89574dd93250091d59929c1f8f1ea2e43da4de
Nino GUEGUEN <ninogueguen@gmail.com>
2023-12-05 19:33:43 +0100

    ajout db.js suite pb requêtes API, Réalisation de la navbar flottante et du tri des colonnes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import HeaderInfos from "./components/HeaderInfos";
 import GlobalChart from "./components/GlobalChart";
 import axios from "axios";
 import Table from "./components/Table";
+import { data } from "./db.js";
+import ToTop from "./components/ToTop.js";
 
 const App = () => {
   const [coinsData, setCoinsData] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y"
-      )
-      .then((res) => setCoinsData(res.data));
+    setCoinsData(data); // Utilisez les données importées pour définir l'état
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 145) {
+        document.querySelector(".table-header").classList.add("active");
+      } else {
+        document.querySelector(".table-header").classList.remove("active");
+      }
+    });
   }, []);
+
+  // useEffect(() => {
+  //   .get(
+  //     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y"
+  //   )
+  //   .then((res) => setCoinsData(res.data));
+  // }, []);
+
   return (
     <div className="app-container">
       <header>
@@ -21,6 +35,7 @@ const App = () => {
         <GlobalChart coinsData={coinsData} />
       </header>
       <Table coinsData={coinsData} />
+      <ToTop />
     </div>
   );
 };
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import TableLine from "./TableLine";
+import ToTop from "./ToTop";
 
 const Table = ({ coinsData }) => {
   const [rangeNumber, setRangeNumber] = useState(100);
@@ -37,6 +38,7 @@ const Table = ({ coinsData }) => {
             value={rangeNumber}
             onChange={(e) => setRangeNumber(e.target.value)}
           />
+          <ToTop />
         </div>
         {tableHeader.map((el) => (
           <li key={el}>
@@ -63,6 +65,91 @@ const Table = ({ coinsData }) => {
       {coinsData &&
         coinsData
           .slice(0, rangeNumber)
+          .sort((a, b) => {
+            switch (orderBy) {
+              case "Prix":
+                return a.current_price - b.current_price;
+              case "Prixreverse":
+                return b.current_price - a.current_price;
+              case "MarketCap":
+                return a.market_cap - b.market_cap;
+              case "MarketCapreverse":
+                return b.market_cap - a.market_cap;
+              case "Volume":
+                return a.total_volume - b.total_volume;
+              case "Volumereverse":
+                return b.total_volume - a.total_volume;
+              case "1h":
+                return (
+                  a.price_change_percentage_1h_in_currency -
+                  b.price_change_percentage_1h_in_currency
+                );
+              case "1hreverse":
+                return (
+                  b.price_change_percentage_1h_in_currency -
+                  a.price_change_percentage_1h_in_currency
+                );
+              case "1j":
+                return (
+                  a.price_change_percentage_24h_in_currency -
+                  b.price_change_percentage_24h_in_currency
+                );
+              case "1jreverse":
+                return (
+                  b.price_change_percentage_24h_in_currency -
+                  a.price_change_percentage_24h_in_currency
+                );
+              case "7j":
+                return (
+                  a.price_change_percentage_7d_in_currency -
+                  b.price_change_percentage_7d_in_currency
+                );
+              case "7jreverse":
+                return (
+                  b.price_change_percentage_7d_in_currency -
+                  a.price_change_percentage_7d_in_currency
+                );
+              case "1m":
+                return (
+                  a.price_change_percentage_30d_in_currency -
+                  b.price_change_percentage_30d_in_currency
+                );
+              case "1mreverse":
+                return (
+                  b.price_change_percentage_30d_in_currency -
+                  a.price_change_percentage_30d_in_currency
+                );
+              case "6m":
+                return (
+                  a.price_change_percentage_200d_in_currency -
+                  b.price_change_percentage_200d_in_currency
+                );
+              case "6mreverse":
+                return (
+                  b.price_change_percentage_200d_in_currency -
+                  a.price_change_percentage_200d_in_currency
+                );
+              case "1an":
+                return (
+                  a.price_change_percentage_1y_in_currency -
+                  b.price_change_percentage_1y_in_currency
+                );
+              case "1anreverse":
+                return (
+                  b.price_change_percentage_1y_in_currency -
+                  a.price_change_percentage_1y_in_currency
+                );
+
+              case "ATH":
+                return a.ath_change_percentage - b.ath_change_percentage;
+
+              case "ATHreverse":
+                return b.ath_change_percentage - a.ath_change_percentage;
+              default:
+                return null;
+            }
+          })
+
           .map((coin, index) => <TableLine coin={coin} index={index} />)}
     </div>
   );
